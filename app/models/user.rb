3be856166edcab815827a5144abc18e765eddc7f
Toomah <fsooj@qatar.cmu.edu>
2016-04-23 18:05:33 +0000

    saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,9 @@ class User < ActiveRecord::Base
     belongs_to :role
 
     validates :name, presence: true, length: { maximum: 50 }
-   # validates :admin ,:dependent => :delete_all
-    validates :password, presence: true, length: { minimum: 2 }
+    validates :password, presence: true, length: { minimum: 5 }
     
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-      validates :email, presence: true, length: { maximum: 255 },
-      format: { with: VALID_EMAIL_REGEX }
-      
       validates :email, presence: true, length: { maximum: 255 },
       format: { with: VALID_EMAIL_REGEX },
       uniqueness: { case_sensitive: false }
@@ -18,8 +14,8 @@ class User < ActiveRecord::Base
       
       has_secure_password
       
-      validates :email, presence: true, length: { maximum: 255 },
-format: { with: VALID_EMAIL_REGEX },
-uniqueness: { case_sensitive: false }
+
 
 end
+
+
--- test/user_test.rb
+++ test/user_test.rb
@@ -2,11 +2,36 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   
-  def setup
-   @user = User.new(name: "Tartan", email: "tartan@cmu.edu")
-  end
+  
+   def setup
+    @user = User.new(name: "tartan", email: "tartan@cmu.edu", password: "foobar",
+    password_confirmation: "foobar")
+   end
+   
 
   test "should be valid" do
    assert @user.valid?
   end
+  
+    test "email validation should accept valid addresses" do
+   valid_addresses = %w[user@cmu.edu user@qatar.cmu.edu  user@example.conm]
+   valid_addresses.each do |valid_address|
+   @user.email = valid_address
+   assert @user.valid?, "#{valid_address.inspect} should be valid"
+   end
+  end
+  
+   test "email addresses should be unique" do
+     duplicate_user = @user.dup
+     duplicate_user.email = @user.email.upcase
+     @user.save
+     assert_not duplicate_user.valid?
+    end
+  
+   
+   test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+   end
+   
 end
