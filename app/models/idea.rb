1fc8892b88a34ccc4076aede3c1db5d76f470367
Toomah <fsooj@qatar.cmu.edu>
2016-04-24 12:28:39 +0000

    saving

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,16 +1,14 @@
 class Idea < ActiveRecord::Base
    
-   belongs_to:platform
+   belongs_to:platform 
     has_many :users, through: :ideas_users
     has_many :ideas_users, through: :ideas_users
 
-    validates :name, :user_id, :description, :help_needed, :platform_id,:developer,:designer, presence: true
+    validates :name, :description, :help_needed, :platform_id, presence: true
    # validates :designer, :developer, default: 0
     # :ascending, -> { order(ideas.created_at ASC) }
     scope :ascending, -> {order('name asc')}
     scope :descending, -> {order('name desc')}
-
-  #  scope :open, -> {where(filled: false)}
 
     
 end
--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,4 +1,4 @@
 class Platform < ActiveRecord::Base
-    has_many :idea
+    has_many :ideas
     validates :name, presence: true, uniqueness: true
 end
